3db80cec6642ac0528d557ff13635ab915cb53f9
Ramsundar Karthisan S <iamsrsk2004@gmail.com>
2024-03-01 17:04:16 +0530

    Update calculator_using_stack.java

--- Stack/calculator_using_stack.java
+++ Stack/calculator_using_stack.java
@@ -1,81 +1,46 @@
 import java.util.*;
-public class Main {
-    public static boolean isOperator(char c){
-        return c=='+' || c=='-' || c=='*' || c=='/' || c=='^';
-    }
-    public static int getPrecedence(char c){
-        if(c=='+' || c=='-') return 1;
-        else if(c=='*' || c=='/' || c=='%') return 2;
-        else if(c=='^') return 3;
-        else return 0;
-    }
-    
-    public static String infixToPostfix(String infix){
-        String postfix = "";
-        int len = infix.length();
-        Stack<Character> s = new Stack<>();
-        char c;
-        for(int i = 0; i<len; i++){
-            c = infix.charAt(i);
-            if(isOperator(c)){
-                while(!s.empty() && getPrecedence(c)<=getPrecedence(s.peek())){
-                    postfix+=s.peek();
-                    s.pop();
-                }
-                s.push(c);
-            }
-            else if(c=='('){
-                s.push(c);
-            }
-            else if(c==')'){
-                while(!s.empty() && s.peek()!='('){
-                    postfix+=s.peek();
-                    s.pop();
-                }
-                s.pop();
-            }
-            else postfix+=c;
+
+class Main
+{
+  public static int calculate(String s) {
+    Stack<Integer> stack = new Stack<Integer>();
+    int result = 0;
+    int number = 0;
+    int sign = 1;
+    for(int i = 0; i < s.length(); i++){
+        char c = s.charAt(i);
+        if(Character.isDigit(c)){
+            number = 10 * number + (int)(c - '0');
+        }else if(c == '+'){
+            result += sign * number;
+            number = 0;
+            sign = 1;
+        }else if(c == '-'){
+            result += sign * number;
+            number = 0;
+            sign = -1;
+        }else if(c == '('){
+            //we push the result first, then sign;
+            stack.push(result);
+            stack.push(sign);
+            //reset the sign and result for the value in the parenthesis
+            sign = 1;   
+            result = 0;
+        }else if(c == ')'){
+            result += sign * number;  
+            number = 0;
+            result *= stack.pop();    //stack.pop() is the sign before the parenthesis
+            result += stack.pop();   //stack.pop() now is the result calculated before the parenthesis
+            
         }
-        while(!s.empty()){
-            if(s.peek()=='(') return "Invalid Expression";
-            postfix += s.peek();
-            s.pop();
-        }
-        return postfix;
     }
-    
-    public static int posteval(String inp){
-        Stack<Integer> stk = new Stack<>();
-        int res = 0;
-        for (int i = 0; i < inp.length(); i++) {
-            char inpchar = inp.charAt(i);
-            if (!isOperator(inpchar)) {
-                int inpint = Integer.parseInt(String.valueOf(inp.charAt(i)));
-                stk.push(inpint);
-            } else {
-                int temp = stk.pop();
-                int temp2 = stk.pop();
-                if (inpchar == '+') {
-                    res = temp + temp2;
-                } else if (inpchar == '-') {
-                    res = temp2 - temp;
-                } else if (inpchar == '*') {
-                    res = temp * temp2;
-                } else if (inpchar == '/') {
-                    res = temp2 / temp;
-                } else if (inpchar == '^') {
-                    res = temp2 ^ temp;
-                }
-                stk.push(res);
-            }
-        }
-        return res;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String infix = sc.next();
-        String postfix = infixToPostfix(infix);
-        int ans = posteval(postfix);
-        System.out.println(ans);
-    }
+    if(number != 0) result += sign * number;
+    return result;
+}
+  public static void main(String args[])
+  {
+    //Try out your code here
+    Scanner sc = new Scanner(System.in);
+    System.out.println(calculate(sc.nextLine()));
+  }
 }
